611cfbc479c6dde70d9d551e6b7ef7c84037defa
Yuto Takano <moa17stock@gmail.com>
2024-05-15 23:46:35 +0100

    Remove window-controls and use native ones
    While transparent/overlaid title bars are extremely attractive, they
    are impossible to get right in the current state of Tauri. The
    window-controls package attempts to recreate the native design with
    HTML/CSS, but obviously lacks native controls (snap layout, etc).
    
    Tauri supports native overlaid title bars using titleBarStyle: Overlay
    but this is not supported in Windows/Linux so using it is more painful
    for maintenance than not.

--- app/src/main.rs
+++ app/src/main.rs
@@ -3,7 +3,6 @@
 
 fn main() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_os::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,8 @@
-import { WindowTitlebar } from "tauri-controls";
 import { Button } from "./components/ui/button";
 
 function App() {
 	return (
 		<>
-			<WindowTitlebar>title</WindowTitlebar>
 			<Button>click me </Button>
 		</>
 	);
