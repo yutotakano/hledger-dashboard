9ac324e29194f4d2584dba3ad13916c27a2bb8f5
Yuto Takano <moa17stock@gmail.com>
2024-05-15 22:46:12 +0100

    Add webview-based window controls, migrate to Tauri v2 beta as required

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
+import { WindowTitlebar } from "tauri-controls";
 import { Button } from "./components/ui/button";
 
 function App() {
 	return (
 		<>
+			<WindowTitlebar>title</WindowTitlebar>
 			<Button>click me </Button>
 		</>
 	);
