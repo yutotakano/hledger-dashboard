126a21de4dde7beb7ad503546d4c436bbb2d475c
Yuto Takano <moa17stock@gmail.com>
2024-05-16 00:08:48 +0100

    Implement simple two-column grid layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,12 @@ import { Button } from "./components/ui/button";
 function App() {
 	return (
 		<>
-			<Button>click me </Button>
+			<div className="grid w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+				<div className="flex h-full">test</div>
+				<div className="flex">
+					<Button>click me </Button>
+				</div>
+			</div>
 		</>
 	);
 }
