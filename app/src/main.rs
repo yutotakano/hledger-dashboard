43137f4b67308057891ba2f6c87e4ee393abe256
Yuto Takano <moa17stock@gmail.com>
2024-05-16 14:12:08 +0100

    Use window-vibrancy to achieve Mica in Windows

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,8 +1,23 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+use window_vibrancy::{apply_tabbed, apply_vibrancy, NSVisualEffectMaterial};
+
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            let window = app.get_webview_window("main").unwrap();
+            let _ = window.set_title("Hello World");
+            #[cfg(target_os = "macos")]
+            apply_vibrancy(&window, NSVisualEffectMaterial::HudWindow, None, None)
+                .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+
+            #[cfg(target_os = "windows")]
+            apply_tabbed(&window, Some(false))
+                .expect("Unsupported platform! 'apply_mica' is only supported on Windows");
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
