fccc0c7f648300ff078ad8c0fcf59d4ab94b73dc
Yuto Takano <moa17stock@gmail.com>
2024-05-16 22:13:44 +0100

    Modify biome formatter config to use spaces over tabs in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,44 +4,44 @@ import { useEffect, useState } from "react";
 import type { Event } from "@tauri-apps/api/event";
 
 const useOnFocusChanged = (callback: (focused: boolean) => void) => {
-	// Use a listener for appWindow.onFocusChanged, and clean it up
-	// when the component unmounts. The Tauri IPC is only available to listen on
-	// after the window object is available, which we ensure by using useEffect,
-	// which only runs after renders (+ when variables are changed).
-	useEffect(() => {
-		const onFocusChanged = (focused: Event<boolean>) => {
-			callback(focused.payload);
-		};
+  // Use a listener for appWindow.onFocusChanged, and clean it up
+  // when the component unmounts. The Tauri IPC is only available to listen on
+  // after the window object is available, which we ensure by using useEffect,
+  // which only runs after renders (+ when variables are changed).
+  useEffect(() => {
+    const onFocusChanged = (focused: Event<boolean>) => {
+      callback(focused.payload);
+    };
 
-		const unlisten = getCurrent().onFocusChanged(onFocusChanged);
+    const unlisten = getCurrent().onFocusChanged(onFocusChanged);
 
-		return () => {
-			unlisten.then((result) => {
-				if (!result) {
-					console.error("Failed to unlisten onFocusChanged");
-				}
-			});
-		};
-	}, [callback]);
+    return () => {
+      unlisten.then((result) => {
+        if (!result) {
+          console.error("Failed to unlisten onFocusChanged");
+        }
+      });
+    };
+  }, [callback]);
 };
 
 function App() {
-	const [focused, setFocused] = useState(false);
-	useOnFocusChanged(setFocused);
+  const [focused, setFocused] = useState(false);
+  useOnFocusChanged(setFocused);
 
-	const main_card_bg = focused ? "bg-opacity" : "bg-opacity-90";
-	return (
-		<>
-			<div className="grid grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr] transition-[grid-template-columns] h-full p-1">
-				<div className="flex h-full">test</div>
-				<div
-					className={`flex h-full rounded-md p-1 shadow-md transition-colors bg-white ${main_card_bg}`}
-				>
-					<Button>click me </Button>
-				</div>
-			</div>
-		</>
-	);
+  const main_card_bg = focused ? "bg-opacity" : "bg-opacity-90";
+  return (
+    <>
+      <div className="grid grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr] transition-[grid-template-columns] h-full p-1">
+        <div className="flex h-full">test</div>
+        <div
+          className={`flex h-full rounded-md p-1 shadow-md transition-colors bg-white ${main_card_bg}`}
+        >
+          <Button>click me </Button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,11 +5,11 @@ import "./index.css";
 
 const root = document.getElementById("root");
 if (!root) {
-	throw new Error("Root element not found, check index.html?");
+  throw new Error("Root element not found, check index.html?");
 }
 
 ReactDOM.createRoot(root).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
