0f1c8c785f2eca9cf524b73fda6e1fac17dfd1f3
Yuto Takano <moa17stock@gmail.com>
2024-05-17 05:36:50 +0100

    Remove set_title caled from Rust on launch

--- app/src/main.rs
+++ app/src/main.rs
@@ -13,7 +13,6 @@ fn main() {
     tauri::Builder::default()
         .setup(|app| {
             let window = app.get_webview_window("main").unwrap();
-            let _ = window.set_title("Hello World");
             #[cfg(target_os = "macos")]
             apply_vibrancy(&window, NSVisualEffectMaterial::HudWindow, None, None)
                 .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
